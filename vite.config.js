928d4cd430d0b677d781d102857393e7aaed694d
Ankit Shiv <ragnarok.infinity.butterfly@gmail.com>
2026-06-01 16:55:07 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import babel from '@rolldown/plugin-babel'
 export default defineConfig({
   plugins: [
     react(),
-    base: '/',
     babel({ presets: [reactCompilerPreset()] })
   ],
+  base: '/',
 })
